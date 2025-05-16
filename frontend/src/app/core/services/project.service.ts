ae31efd82d595cc49feab3aeed0e1236757af9fd
WalidBenTouhami <walid.bentouhami@esprit.tn>
2025-05-16 19:20:20 +0100

    update

--- frontend/src/app/core/services/project.service.ts
+++ frontend/src/app/core/services/project.service.ts
@@ -1,11 +1,32 @@
 //src/app/core/services/project.service.ts
 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ProjectService {
+  private baseUrl = `${environment.apiUrl}/projects`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProjects() {
+    return this.http.get(this.baseUrl);
+  }
+
+  getProject(id: string) {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createProject(data: any) {
+    return this.http.post(this.baseUrl, data);
+  }
+
+  updateProject(id: string, data: any) {
+    return this.http.put(`${this.baseUrl}/${id}`, data);
+  }
+
+  deleteProject(id: string) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
 }
